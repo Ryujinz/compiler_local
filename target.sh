83ce52fbea45448c6585ecb667cd1d6c35c89286
Renaldi_GP <39940824+Ryujinz@users.noreply.github.com>
2022-01-09 18:35:00 +0700

    Update Path

--- target.sh
+++ target.sh
@@ -4,7 +4,7 @@
 
 # Main
 KERNEL_ROOTDIR=~/kranul # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG=vendor/RMX1911_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+DEVICE_DEFCONFIG=vayu_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
 CLANG_ROOTDIR=~/proton # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=renaldigp # Change with your own name or else.
-export KBUILD_BUILD_HOST=ryujinz # Change with your own hostname.
+export KBUILD_BUILD_HOST=rex # Change with your own hostname.
