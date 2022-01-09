34e9ed30f4344c4b5f73dd31f0c7681dfe0e107f
Renaldi_GP <39940824+Ryujinz@users.noreply.github.com>
2022-01-09 18:33:08 +0700

    Update Script

--- compile.sh
+++ compile.sh
@@ -4,8 +4,14 @@
 
 # Warning !! Dont Change anything there without known reason.
 cd ${KERNEL_ROOTDIR}
-make -j$(nproc --all) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-make -j$(nproc --all) ARCH=arm64 O=out \
+  make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+  make -j$(nproc) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
 	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi- \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	LD=ld.lld \
+	OBJCOPY=llvm-objcopy \
+	OBJDUMP=llvm-objdump \
+	STRIP=llvm-strip \
